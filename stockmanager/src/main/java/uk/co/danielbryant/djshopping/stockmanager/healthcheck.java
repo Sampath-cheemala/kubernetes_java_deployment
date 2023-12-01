d2aa7aa93a38e994d815358ec415ecb641ffef34
VS Charan <138986928+VootlaSaiCharan@users.noreply.github.com>
2023-12-01 08:10:06 +0530

    Update healthcheck.java

--- stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
+++ stockmanager/src/main/java/uk/co/danielbryant/djshopping/stockmanager/healthcheck.java
@@ -1,4 +1,4 @@
-package com.example.springbootmanagementexample;
+package uk.co.danielbryant.djshopping.stockmanager;
  
 import java.util.Date;
 import org.springframework.web.bind.annotation.GetMapping;
